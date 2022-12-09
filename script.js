eae19a1cce38326c545d8b78798d4ed098636653
louis-funes <112581433+louis-funes@users.noreply.github.com>
2022-12-09 18:48:20 -0500

    commit 2

--- script.js
+++ script.js
@@ -1,47 +1,41 @@
-
-
-
-
-
 // Function that fecths the UR
 async function gettingURL() {
-  const topicName = document.getElementById("search").value;
+  const topicName = document.getElementById('search').value;
 
-  const rowNumber = document.getElementById("rows#").value;
+  const rowNumber = document.getElementById('rows#').value;
 
   const queryURL = `https://api.si.edu/openaccess/api/v1.0/search?q=${topicName}&rows=${rowNumber}`;
 
-  const api_key = "EmJdH60tNih9rYDose9Icg92QjX1LQF9Y2LBeZh5";
-  const response = await fetch(queryURL + "&api_key=" + api_key);
+  const api_key = 'EmJdH60tNih9rYDose9Icg92QjX1LQF9Y2LBeZh5';
+  const response = await fetch(`${queryURL}&api_key=${api_key}`);
   // converting to json
   const data = await response.json();
 
-  const gettingrows = await data.response["rows"];
+  const gettingrows = await data.response.rows;
 
   // lenght of the data
   const arrayLenght = gettingrows.length;
 
   // creating and stroing the data in for loop
-  let findit = document.getElementById("idtest");
-  let arrayCounter = document.getElementById("arrCoun");
+  const findit = document.getElementById('idtest');
+  const arrayCounter = document.getElementById('arrCoun');
 
-  for (var i = 0; i < arrayLenght; i++) {
-    console.log(gettingrows[i]["title"]);
+  for (let i = 0; i < arrayLenght; i++) {
+    console.log(gettingrows[i].title);
 
     console.log(
-      gettingrows[i]["content"]["descriptiveNonRepeating"]["record_link"]
+      gettingrows[i].content.descriptiveNonRepeating.record_link
     );
 
-    const gettingTitle = gettingrows[i]["title"];
+    const gettingTitle = gettingrows[i].title;
 
-    const gettinLinks =
-      gettingrows[i]["content"]["descriptiveNonRepeating"]["record_link"];
+    const gettinLinks = gettingrows[i].content.descriptiveNonRepeating.record_link;
 
     // storing and selecting the data
-    let span = document.createElement("a");
+    const span = document.createElement('a');
 
-    span.id = "span";
-    span.className = "boxData";
+    span.id = 'span';
+    span.className = 'boxData';
     span.href = gettinLinks;
 
     // appending to the findit variable
@@ -49,38 +43,32 @@ async function gettingURL() {
     findit.append(span);
   }
   const gettingLenghtArticles = arrayLenght;
-  const article = " articles";
-  arrayCounter.append(gettingLenghtArticles, article); 
+  const article = ' articles';
+  arrayCounter.append(gettingLenghtArticles, article);
 }
 
-
-function xl() {
-  
+function articleChart() {
   const ctx = document.getElementById('chart');
   new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: 'Articles selected (Order by date = Oldest to Recent)',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: 'Articles selected (Order by date = Oldest to Recent)',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
       }
     }
-  }
-});
+  });
 }
-
 
 // Calling the function fetching
 gettingURL();
-xl()
-
-
-
+articleChart();
